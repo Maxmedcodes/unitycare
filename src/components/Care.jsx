f159c947f44c15f4facd7208ba723fc8e4f1b8c1
Maxmedcodes <70708155+Maxmedcodes@users.noreply.github.com>
2025-03-17 22:49:28 +0000

    Completed Wireframe boxes for Care section

--- src/components/Care.jsx
+++ src/components/Care.jsx
@@ -3,10 +3,20 @@ function Care() {
         <section className="app-benefits">
             <div className="app-benefits-content">
                 <div className="app-benefits-1">
-
+                    <div className="bensec-1">
+                        <h1>Testing bensec 1</h1>
+                    </div>
+                    <div className="bensec-2">
+                        <h1>Testing bensec 2 </h1>
+                    </div>
                 </div>
                 <div className="app-benefits-2">
-
+                    <div className="bensec2-1">
+                        <h1>Testing bensec2  1</h1>
+                    </div>
+                    <div className="bensec2-2">
+                        <h1>Testing bensec2 2</h1>
+                    </div>
                 </div>
             </div>
         </section>
