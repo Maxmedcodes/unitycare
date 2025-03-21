0149be169249f970b52a0d4a6743451c944f65a1
Maxmedcodes <70708155+Maxmedcodes@users.noreply.github.com>
2025-03-21 14:17:28 +0000

    Completed benefits Section

--- src/components/Care.jsx
+++ src/components/Care.jsx
@@ -4,19 +4,46 @@ function Care() {
             <div className="app-benefits-content">
                 <div className="app-benefits-1">
                     <div className="bensec-1">
-                        <h1>Testing bensec 1</h1>
+                        <h1>Bringing Comfort When It Matters Most</h1>
+                        <p>
+                            Unity Care volunteers provide vital social support and compassionate companionship to hospital patients, strengthening connections between patients and their communities, helping those facing illness feel less isolated, and creating meaningful human connections—providing profound benefits for patients, families, and volunteers alike."
+                        </p>
+                        <p>
+                            Through Unity Care, our dedicated volunteers deliver essential emotional support and bedside companionship, bridging the gap between medical care and human connection. By reducing isolation during hospital stays, we help patients maintain their spirits, give families peace of mind, and create rewarding opportunities for volunteers—fostering a community of care that benefits everyone involved.
+                        </p>
                     </div>
-                    <div className="bensec-2">
-                        <h1>Testing bensec 2 </h1>
-                    </div>
+
                 </div>
                 <div className="app-benefits-2">
-                    <div className="bensec2-1">
-                        <h1>Testing bensec2  1</h1>
+                    <div className="benefits-one">
+                        <div className="bensec2-1">
+                            <img className="benefits-images" src="src\assets\images\CareImage1.png" alt="" height={"100px"} width={"100px"} />
+                            <h1>Foster Connection</h1>
+                            <p>
+                                UnityCare empowers individuals to extend support by connecting volunteers with those in need of companionship, especially in hospital settings.
+                            </p>
+                        </div>
+                        <div className="bensec2-2">
+                            <img className="benefits-images" src="src\assets\images\CareImage2.png" alt="" height={"100px"} width={"100px"} />
+                            <h1>Cultivate Compassion and Engagement</h1>
+                            <p> Unitycare provides a platform for people to show compassion, and to be active members of their community.</p>
+                        </div>
                     </div>
-                    <div className="bensec2-2">
-                        <h1>Testing bensec2 2</h1>
+                    <div className="benefits-two">
+                        <div className="bensec2-3">
+                            <img className="benefits-images" src="src\assets\images\CareImage3.png" alt="" height={"100px"} width={"100px"} />
+                            <h1>Enhance Well-being</h1>
+                            <p>
+                                By providing compassionate visits, UnityCare contributes to the emotional and mental well-being of individuals, promoting a sense of support during challenging times
+                            </p>
+                        </div>
+                        <div className="bensec2-4">
+                            <img className="benefits-images" src="src\assets\images\CareImage4.png" alt="" height={"100px"} width={"100px"} />
+                            <h1>Combat Loneliness Through Shared Moments</h1>
+                            <p>UnityCare believes in the power of human connection. Through volunteer visits, we create meaningful moments that combat loneliness and foster a sense of belonging</p>
+                        </div>
                     </div>
+
                 </div>
             </div>
         </section>
