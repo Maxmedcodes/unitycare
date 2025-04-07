18ad2e635fcbcb0b09b141f7cf5ac5618cb718c8
Maxmedcodes <70708155+Maxmedcodes@users.noreply.github.com>
2025-04-07 19:26:24 +0100

    Completed Donwloads page

--- src/Pages/DownloadPage.jsx
+++ src/Pages/DownloadPage.jsx
@@ -8,7 +8,7 @@ function DownloadPage() {
 
         <section className="downloadpage">
             <div className="download-section1" >
-                <div className="ds1-left">
+                <div className="ds1-left-app">
                     <div className="download-header">
                         <h1>Download Our New App</h1>
                         <p>No One Heals Alone</p>
@@ -33,6 +33,38 @@ function DownloadPage() {
                                 className={"applestore"}
                             />
                         </div>
+                    </div>
+                </div>
+                <div className="ds1-right-app">
+                    <img src="src\assets\images\app-image1.png" alt="" />
+                </div>
+            </div>
+            <div className="download-section2">
+
+
+
+                <div className="download-how-header">
+                    <div className="download-header-content">
+                        <p>How it Works</p>
+                        <h1>Get Started in Just</h1>
+                        <h2> <span id='download-three'> Three</span>  Steps</h2>
+                    </div>
+                </div>
+                <div className="download-how-main">
+                    <div className="download-part1">
+                        <h3> Create an Account</h3>
+                        <p>Sign up today and begin your volunteering journey with us. Setting up your profile is the first step to making a difference.</p>
+                        <img src="src\assets\images\app-image2.png" alt="Unity care App  being displayed on an Iphone" />
+                    </div>
+                    <div className="download-part2">
+                        <h3>Volunteer</h3>
+                        <p> Make an impact starting now. Join our community of volunteers bringing compassion and care to every visit.</p>
+                        <img src="src\assets\images\app-image3.png" alt="" />
+                    </div>
+                    <div className="download-part3">
+                        <h3>Request</h3>
+                        <p>Submit customized volunteer requests based on your specific needs and requirements.</p>
+                        <img src="src\assets\images\app-image4.png" alt="" />
                     </div>
                 </div>
             </div>
