5b0d18b0a26cd1298bfce3206b8554145804dc89
Maxmedcodes <70708155+Maxmedcodes@users.noreply.github.com>
2025-05-18 18:19:59 +0100

    Added App Incoming Page

--- src/Pages/AppIncoming.tsx
+++ src/Pages/AppIncoming.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 function AppIncoming() {
   return (
-    <div className='AppIncoming'>AppIncoming</div>
+    <div className="Incoming">
+      <div className='AppIncoming'>
+        <h2>
+        App is Being Built. Its on the Way, check back here Later
+        </h2></div>
+      <div className="incoming-lottie">
+        <DotLottieReact
+            src="src/lotties/AppDev.json"
+            
+            autoplay
+            loop
+            style={{ width: "70%", height: '70%' }}
+        />
+      </div>
+    </div>
+    
   )
 }
 
